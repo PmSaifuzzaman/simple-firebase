cbb26a781a5e71bf043784ce78f559410b8db8ee
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-30 00:28:35 +0600

    google authentication added

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,25 @@
-
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "../../firebase/firebase.init";
 
 const Login = () => {
+    const auth = getAuth(app);
+    console.log(auth);
+    const provider = new GoogleAuthProvider();
+
+    const handleSignIn = () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                const user = result.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                const errorMessege = error.messege;
+                console.log(errorMessege);
+            })
+    }
     return (
         <div>
-            <button>Login Google</button>
+            <button onClick={handleSignIn}>Login Google</button>
         </div>
     );
 };
