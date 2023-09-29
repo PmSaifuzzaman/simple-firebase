987385a0815138173d2875bf1b339b30d25b1532
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-09-29 21:02:34 +0600

    react router added

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div>
+            <button>Login Google</button>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC17_oQDIYmtSdp2RRw3m_jNTedHRIW6JE",
+  authDomain: "simple-firebase-427c4.firebaseapp.com",
+  projectId: "simple-firebase-427c4",
+  storageBucket: "simple-firebase-427c4.appspot.com",
+  messagingSenderId: "685691974664",
+  appId: "1:685691974664:web:efc51a22b657c2dcf90dbd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
